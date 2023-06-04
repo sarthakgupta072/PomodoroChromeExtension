bf47399b7ad5c4bd61d8b1ac25dd02a81e0d6c98
Sarthak Gupta <sarthakgupta072@gmail.com>
2023-06-05 00:20:21 +0530

    feat: addd timer

---
+++ background.js
@@ -0,0 +1,35 @@
+chrome.alarms.create("pomodoroTimer", {
+    periodInMinutes: 1 / 60
+})
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if(alarm.name == "pomodoroTimer") {
+        chrome.storage.local.get(["timer", "isRunning"], (res) => {
+            if(res.isRunning) {
+                let timer = res.timer + 1
+                let isRunning = true
+                console.log(timer)
+                if(timer == 60 * 25) {
+                    console.log("notification")
+                    this.registration.showNotification("Pomodoro Timer", {
+                        body: "25 minutes has passed!",
+                        icon: "icon.png"
+                    })
+                    timer = 0
+                    isRunning = false
+                }
+                chrome.storage.local.set({
+                    timer,
+                    isRunning
+                })
+            }
+        })
+    }
+})
+
+chrome.storage.local.get(["timer", "isRunning"], (res) => {
+    chrome.storage.local.set({
+        timer: "timer" in res ? res.timer : 0,
+        isRunning: "isRunning" in res ? res.isRunning : false
+    })
+})
